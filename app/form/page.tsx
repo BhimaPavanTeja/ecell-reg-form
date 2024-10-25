39999894ef794b1871262b829abebf8e21ea1fcc
pavan <124360829+BhimaPavanTeja@users.noreply.github.com>
2024-10-25 23:24:08 +0530

    update page.tsx

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -4,9 +4,9 @@ const FormPage = () => {
   return (
     <div className="space-y-6 max-w-[640px] mx-auto mt-10">
       <div>
-        <h2 className="text-2xl font-semibold my-2">Register for Eureka!</h2>
+        <h2 className="text-2xl font-semibold my-2">Register for illuminate Workshop!</h2>
         <p className="text-sm text-muted-foreground">
-          Fill out this form to register for Eureka, the biggest event for
+          Fill out this form to register for Workshop, the biggest event for
           student entrepreneurs, proudly presented by the{" "}
           <span className="text-foreground font-medium">ECell REC.</span>
         </p>
